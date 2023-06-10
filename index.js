bf46d50e960a7830819b0c87954e506db3a96f76
simplysabir <simplysabir@gmail.com>
2023-06-10 19:01:57 +0530

    ✨ feat : Database Connectivity Added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const connectToDB = require('./database'); 
 app.use(express.json());
 
+connectToDB()
+    .then(()=>{
+    console.log("Database Connected");
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
 
 app.listen(PORT, ()=>{
     console.log(`Started Listening on Port : ${PORT}`);
